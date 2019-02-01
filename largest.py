e0aaa30a88e6cb0b48a807ec7f9777da1374b593
jhonson1901 <47185772+jhonson1901@users.noreply.github.com>
2019-02-01 11:58:17 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,13 +1,13 @@
 a=int(input())
 b=int(input())
 c=int(input())
-if a>b:
+if a>=b:
   if a>c:
     print(a)
   else:
     print(c)
-if b>c:
-  if b>a:
+elif b>=c:
+  if b>=a:
     print(b)
 else:
   print(c)
