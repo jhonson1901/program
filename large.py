f70cddd49b7344d89fb570b21a237eaba6bcdb15
jhonson1901 <47185772+jhonson1901@users.noreply.github.com>
2019-02-04 11:46:53 +0530

    Update large.py

--- large.py
+++ large.py
@@ -2,9 +2,9 @@ ww=input().split()
 a=int(ww[0])
 b=int(ww[1])
 c=int(ww[2])
-if (a > b) and (a > c):
+if (a >= b) and (a >= c):
    largest = a
-elif (b > a) and (b > c):
+elif (b >= a) and (b >= c):
    largest = b
 else:
    largest = c
